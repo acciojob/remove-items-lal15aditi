f7aac4716310614601ac461b3773abefd75c11a7
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-30 16:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.querySelector('form > input');
+
+btn.addEventListener('click', (event) => {
+	for(let i = 0; i<event.target.parentNode.children[0].children.length; i++){
+		let e = event.target.parentNode.children[0].children[i];
+		if(e.selected) e.remove();
+	}
+})
